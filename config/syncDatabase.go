e8eebb15495c28d6b006adbcd0eb78ed0fb11ce5
orket <developerorket@gmail.com>
2025-01-06 19:21:24 -0500

    test

--- config/syncDatabase.go
+++
@@ -1,7 +0,0 @@
-package config
-
-import "github.com/orket-sam/go-jwt/models"
-
-func SyncDB() {
-	DB.AutoMigrate(&models.User{})
-}
--- main.go
+++ main.go
@@ -1,18 +1,14 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"github.com/orket-sam/go-jwt/config"
-	"github.com/orket-sam/go-jwt/routes"
-)
-
-func init() {
-	config.ConnectDb()
-	config.SyncDB()
+func main() {
+	println("welcome to heartbreak")
+	factorial(5)
 }
 
-func main() {
-	r := gin.Default()
-	routes.AuthRoutes(r)
-	r.Run("localhost:8080")
+func factorial(n int) {
+	result := 1
+	for i := 0; i < n; i++ {
+		result *= (i + 1)
+		println(result)
+	}
 }
